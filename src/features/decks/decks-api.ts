c616fd247fc62c5e67156bc8989ec4f8ac016176
SergeyStarchenko <pdxserg@gmail.com>
2024-09-16 09:31:42 -0700

    start project

---
+++ src/features/decks/decks-api.ts
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+export const instance = axios.create({
+  baseURL: 'https://api.flashcards.andrii.es',
+  headers: {
+    'x-auth-skip': true,
+  },
+})
+export const deckAPI ={
+  getDecks(){
+    return instance.get(`/v2/decks`)
+  }
+
+}
+
