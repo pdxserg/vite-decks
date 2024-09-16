7ab694d30274d32efe0c7d34be3fdfda5720a1ad
SergeyStarchenko <pdxserg@gmail.com>
2024-09-16 09:36:25 -0700

    create useEffect

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,5 +1,9 @@
 import s from './DecksList.module.css'
+import { useEffect } from 'react'
 
 export const DecksList = () => {
+  useEffect(() => {
+
+  }, [])
   return <ul className={s.list}></ul>
 }
